ca46662a29744a069d97dc0348c2c339087054d7
sb011 <smitbhoraniya11@gmail.com>
2023-11-08 15:15:01 +0530

    added error handling

--- Services/ReportService.py
+++ Services/ReportService.py
@@ -14,23 +14,26 @@ from Helper.ReportGeneratorHelper import generate_report
         report_id (str): The id of the report
 """
 def trigger_report():
-    # Create a report
-    report = Reports()
+    try:
+        # Create a report
+        report = Reports()
 
-    # Set the report status to running
-    report.id = uuid.uuid4()
-    report.status = ReportTypes.Running.value
-    report.report_file = f"report_{report.id}.csv"
-    ReportRepository.update_report(report)
+        # Set the report status to running
+        report.id = uuid.uuid4()
+        report.status = ReportTypes.Running.value
+        report.report_file = f"report_{report.id}.csv"
+        ReportRepository.update_report(report)
 
-    # Generate the report
-    url = generate_report(report)
+        # Generate the report
+        url = generate_report(report)
 
-    # Update the report to completed
-    report.url = url
-    report.status = ReportTypes.Completed.value
-    ReportRepository.update_report(report)
-    return {"report_id": report.id}
+        # Update the report to completed
+        report.url = url
+        report.status = ReportTypes.Completed.value
+        ReportRepository.update_report(report)
+        return {"report_id": report.id}
+    except:
+        return {"Error": "Report generation failed"}
 
 """
     This function is used to get the report by id.
@@ -42,14 +45,17 @@ def trigger_report():
         report (Reports): The report
 """
 def get_report(report_id: str):
-    report = ReportRepository.get_report(report_id)
+    try:
+        report = ReportRepository.get_report(report_id)
 
-    # Check if the report exists
-    if report is None:
-        return {"Error": "Report not found"}
-    
-    # Check if the report is running
-    if report.status == "Running":
-        return {"status": "Running"}
-    
-    return {"status": "completed", "report": report.url}
+        # Check if the report exists
+        if report is None:
+            return {"Error": "Report not found"}
+        
+        # Check if the report is running
+        if report.status == "Running":
+            return {"status": "Running"}
+        
+        return {"status": "completed", "report": report.url}
+    except:
+        return {"Error": "Get report failed"}
